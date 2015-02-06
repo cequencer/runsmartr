a544f1ebc4713708f5dd011f1166530bee985360
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2015-02-06 00:31:30 -0800

    Improved UI.

---
+++ app/static/js/runsmartr.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    sizeMap();
+    $(window).resize(sizeMap);
+    addMap();
+    $("#address").geocomplete();
+});
+
+function sizeMap(){
+    $("#map").height($("body").height());
+}
+
+function addMap(){		// Mapbox.js map
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYW5keWFsbWFuZGh1bnRlciIsImEiOiJLOUh5aS1vIn0.zFS519161_PwwjqlyMWezA';
+    var map = L.mapbox.map('map', 'andyalmandhunter.l52ai08p', {
+	zoomControl: false
+    }).setView([37.765, -122.441], 13);
+    map.addControl(
+	L.control.zoom().setPosition('bottomleft')
+    );
+}
