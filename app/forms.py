c00043f325acf6d019e648f855041b76bba46269
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2015-02-05 16:23:57 -0800

    Adding flask forms

--- app/forms.py
+++ app/forms.py
@@ -3,4 +3,4 @@ from wtforms import StringField, DecimalField
 from wtforms.validators import DataRequired
 
 class InputForm(Form):
-    address = StringField('openid', validators=[DataRequired()])
+    address = StringField('address', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
 from app import app
+from .forms import InputForm
 from app.runsmartr.runrouter import RunRouter
 
 # rr = RunRouter()
@@ -7,14 +8,11 @@ from app.runsmartr.runrouter import RunRouter
 @app.route('/')
 @app.route('/index')
 def cities_input():
-    return render_template("index.html")
+    return render_template('base.html')
 
-# @app.route('/output')
-# def cities_output():
-#     address = request.args.get('ADDR')
-#     distance = int(request.args.get('DIST'))
-#     rr.do_route(address, distance*1600)
-#     rr.update_folium_map()
-#     rr.run_map.create_map(path='app/templates/stamen_toner.html')
-#     return render_template("output.html", address=address,
-#                            distance=('%d' % distance))
+@app.route('/input', methods=['GET', 'POST'])
+def input():
+    form = InputForm()
+    return render_template('input.html',
+                           title='Input',
+                           form=form);
