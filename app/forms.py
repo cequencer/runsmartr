e2ecb6e6ea0c81e1300fdc6dcb9487bf980f43ae
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2015-02-06 02:42:51 -0800

    Improving UI

--- app/forms.py
+++ app/forms.py
@@ -5,4 +5,6 @@ from wtforms.validators import DataRequired
 class InputForm(Form):
     address = StringField('address', validators=[DataRequired()])
     distance = DecimalField('distance', validators=[DataRequired()])
-    units = RadioField('units', choices=[('mi', 'miles'), ('km', 'km')])
+    units = RadioField('units',
+                       choices=[('mi', 'miles'), ('km', 'km')],
+                       default='mi')
--- app/static/js/runsmartr.js
+++ app/static/js/runsmartr.js
@@ -13,7 +13,7 @@ function addMap(){		// Mapbox.js map
     L.mapbox.accessToken = 'pk.eyJ1IjoiYW5keWFsbWFuZGh1bnRlciIsImEiOiJLOUh5aS1vIn0.zFS519161_PwwjqlyMWezA';
     var map = L.mapbox.map('map', 'andyalmandhunter.l52ai08p', {
 	zoomControl: false
-    }).setView([37.765, -122.441], 13);
+    }).setView([37.771, -122.409], 15);
     map.addControl(
 	L.control.zoom().setPosition('bottomleft')
     );
