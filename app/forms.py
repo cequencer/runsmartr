385c09f521f8e4e98bae23e778df09727206dbf3
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2015-02-05 20:33:31 -0800

    Website

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, DecimalField
+from wtforms import StringField, DecimalField, RadioField
 from wtforms.validators import DataRequired
 
 class InputForm(Form):
     address = StringField('address', validators=[DataRequired()])
+    distance = DecimalField('distance', validators=[DataRequired()])
+    units = RadioField('units', choices=[('mi', 'miles'), ('km', 'km')])
