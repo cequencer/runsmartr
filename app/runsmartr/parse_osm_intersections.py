e46d658c0a9548cf965a4ce88862f3fc47aae534
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2015-02-08 17:14:45 -0800

    Changed parsing of edges

--- app/runsmartr/parse_osm_intersections.py
+++ app/runsmartr/parse_osm_intersections.py
@@ -28,6 +28,7 @@ def parse_osm_intersections_only():
         print 'Done with way %d of %d.' % (count, nways)
         count += 1
 
+# ------------------------------------------------------------------------------
 def add_routing_entry_int(rh_db, node, edge):
     query = """
 INSERT INTO rnodes_intersections (id, point)
@@ -45,10 +46,7 @@ UPDATE routing_intersections SET edges = edges || '{%d}'
 WHERE node = '%d';""" % (edge, node)
     rh_db.db_cur.execute(query)
     rh_db.db_conn.commit()
-
-def get_node_routing_int(rh_db, node):
-    return rh_db.query_raw(
-        "SELECT edges FROM routing_intersections WHERE node = '%d';" % node)
+# ------------------------------------------------------------------------------
 
 def get_nodes_intersections(rh_db, way):
     nodes = rh_db.query_raw("""
@@ -57,6 +55,10 @@ SELECT way_nodes FROM
     (SELECT unnest(nodes) AS way_nodes FROM ways WHERE id = %d) AS way_nodes_table
 WHERE node = way_nodes AND array_length(edges, 1) > 2;""" % (way,))
     return [int(n) for node in nodes for n in node]
+
+def get_node_routing_int(rh_db, node):
+    return rh_db.query_raw(
+        "SELECT edges FROM routing_intersections WHERE node = '%d';" % node)
 
 def get_foot_ways(rh_db, foot_way_types):
     query = """
