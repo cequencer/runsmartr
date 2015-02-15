86b4528490fa3f95a1d99d308bc3db75875aea87
andyalmandhunter <andrewalmandhunter@gmail.com>
2015-02-15 17:55:20 +0000

    Added some AJAX

--- app/views.py
+++ app/views.py
@@ -29,15 +29,33 @@ def run_output():
                         units=form.units.data))
     form.address.data = request.args.get('address')
     form.distance.data = request.args.get('distance')
-    units = request.args.get('units')
     form.units.data = units
+    center_latlon = '%f, %f' % router.data.start
+
+    return render_template(
+        'output.html',
+        center_latlon=center_latlon)
+        
+@all.route('/route', methods=['POST'])
+def run_route():
+    address = request.args.get('address')
+    units = request.args.get('units')
     fac_units = {'km': 1000,
                  'mi': 1608}
     distance = (float(request.args.get('distance')) *
                 fac_units[units])
-    address = request.args.get('address')
     router = RunRouter(address, distance)
-    latlon_string = '%f, %f' % router.data.start
+    router.do_route()
+    route = router.data.detailed_path_latlon(router.current_route)
+    route_length = (router.get_route_length(router.current_route) /
+                    fac_units[units])
+    units_str = {'km': 'km', 'mi': 'mile'}
+    route_json = "{'distance':%f,'route':%s}" % (distance, route)
+    return route_json
+
+@app.route('/runscore', methods=['POST'])
+def run_score():
+    router = RunRouter(address, distance)
     foot_graph = router.data.foot_graph_latlon()
     run_score = [edge['run_score'] for edge in foot_graph]
     min_score = min(run_score)
@@ -45,15 +63,3 @@ def run_output():
     edges = [{'edge': edge['edge'],
               'weight': 1 + 9*(edge['run_score']-min_score) / (max_score-min_score)}
              for edge in foot_graph]
-    router.do_route()
-    route = router.data.detailed_path_latlon(router.current_route)
-    route_length = (router.get_route_length(router.current_route) /
-                    fac_units[units])
-    units_str = {'km': 'km', 'mi': 'mile'}
-    return render_template(
-        'output.html',
-        form=form,
-        center_latlon=latlon_string,
-        route_length=('%.1f ' % route_length) + units_str[units],
-        edges=edges,
-        route=route)
