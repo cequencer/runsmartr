ece861acef0e7971b8957fb30a05333472598a3b
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2015-02-16 16:29:18 -0800

    Getting next neighborhood

--- app/runsmartr/get_routes_by_neighborhood.py
+++ app/runsmartr/get_routes_by_neighborhood.py
@@ -6,22 +6,25 @@ import json
 import psycopg2
 from credentials import cred
 
+import pdb
+
 def main():
     # Use get_mmf_token.py to get a new authorization if necessary
-    headers = {'Api-Key': 'nsnxf9ptznw33fzh59wrdezt2rm3fnkt',
-               'Authorization': 'Bearer b1b0e46c53ec8bbb2c8cebdcd9d2d765734108b1',
-               'Content-Type': 'application/json'}
+    headers = cred['mmf']
+    # headers = {'Api-Key': 'nsnxf9ptznw33fzh59wrdezt2rm3fnkt',
+    #            'Authorization': 'Bearer b1b0e46c53ec8bbb2c8cebdcd9d2d765734108b1',
+    #            'Content-Type': 'application/json'}
     # Get list of relevant route IDs
     conn = psycopg2.connect(**cred['db'])
     cur = conn.cursor()
     cur.execute("""
 SELECT mmf_id FROM mmf_routes, neighborhoods
-WHERE ST_Within(point, polygon) AND name = 'Marina';""")
+WHERE ST_Within(point, polygon) AND name = 'Presidio';""")
     route_IDs = [ el[0] for el in cur.fetchall() ]
     # Loop through these routes, fetching each one
     count = 0
     for id in route_IDs:
-        cur.execute("SELECT * FROM routes_marina WHERE mmf_id = %s;",
+        cur.execute("SELECT * FROM routes_presidio WHERE mmf_id = %s;",
                     (id,))
         if cur.fetchall() == []:
             # Call the mapmyrun API for a lat,long and get some route start points
@@ -29,18 +32,21 @@ WHERE ST_Within(point, polygon) AND name = 'Marina';""")
             payload = {'field_set': 'detailed',
                        'format': 'json'}
             route = requests.get(url, params=payload, headers=headers)
-            route_points = ([ (el['lng'], el['lat'])
-                              for el in route.json()['points'] ])
-            if len(route_points) >= 2:
-                # Convert coordinates to Shapely object
-                route_linestring = LineString(route_points)
-                # Insert into database table
-                linestring = 'ST_GeomFromText(%s, 4326)' % (route_linestring.wkt)
-                cur.execute("""
-INSERT INTO routes_marina (mmf_id, linestring)
-VALUES (%s,ST_GeomFromText(%s,4326));""", (id, route_linestring.wkt))
-                conn.commit()
-                print 'done with ID %d (%d of %d)' % (id, count, len(route_IDs))
+            if route.status_code == 200:
+                route_points = ([ (el['lng'], el['lat'])
+                                  for el in route.json()['points'] ])
+                if len(route_points) >= 2:
+                    # Convert coordinates to Shapely object
+                    route_linestring = LineString(route_points)
+                    # Insert into database table
+                    linestring = 'ST_GeomFromText(%s, 4326)' % (route_linestring.wkt)
+                    cur.execute("""
+                        INSERT INTO routes_presidio (mmf_id, linestring)
+                        VALUES (%s,ST_GeomFromText(%s,4326));""", (id, route_linestring.wkt))
+                    conn.commit()
+                    print 'done with ID %d (%d of %d)' % (id, count, len(route_IDs))
+            else:
+                print "failed to fetch route %d, status %d" % (id, route.status_code)
         else:
             print 'already did ID %d (%d of %d)' % (id, count, len(route_IDs))
         count += 1
