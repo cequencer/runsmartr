cf0dd4924d32384c9a3d7e3c749ab3ff711fae5f
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2015-02-25 23:20:51 -0800

    Added favicon

--- app/runsmartr/runrouter.py
+++ app/runsmartr/runrouter.py
@@ -8,37 +8,12 @@ class RunRouter:
         self.t = 0
         self.alpha = alpha
 
-    def _eta(self):
-        return self.eta0 / (self.alpha * self.t)
-
-    def _eval_grad(self):
-        # Evaluate gradient by picking one point near each of the
-        # current controls, and moving the controls to those points
-        # (vec := this move).
-        # The approximate gradient is (delta cost)/|vec| along (vec) 
-        return
-
-    def _step(self):
-        # Evaluate approximate gradient, then take step
-        new_cost = self.current_cost
-        while new_cost >= self.current_cost:
-            print 'try - dist'
-            new_nodes = self._step_trial(self.nodes)
-            result, new_route = self._get_route(new_nodes)
-            if result == 'success':
-                new_cost = self._obj_fn(new_route)
-        self.current_cost = new_cost
-        self.nodes = new_nodes
-        self.current_route = new_route
-
     def _initialize_search(self):
-        # Currently finds random node near top-scored edge.  This is
-        # slow and probably not the best starting point ...
         G = self.data.foot_graph
         run_scores = [float(G.get_edge_data(u,v)['run_score']) for u,v in G.edges()]
         top_node = G.edges()[run_scores.index(max(run_scores))][0]
         self.nodes = [self.data.start_node, 0, 0]
-        result = 'failed'
+        result = 'first_try'
         while result != 'success':
             self.nodes[1] = self.data.rand_rnode_within_n(
                 top_node, 40)
@@ -47,10 +22,25 @@ class RunRouter:
             result, self.current_route = self._get_route(self.nodes)
         self.current_cost = self._obj_fn(self.current_route)
 
-    # Is it much faster to just find the shortest path length than to
-    # find all the nodes in the path?  Probably not ... anyway, need
-    # to find the whole path in order to eliminate these nodes from
-    # graph.
+    def _eta(self):
+        return self.eta0 / (self.alpha * self.t)
+
+    def _eval_grad(self):
+        return
+
+    def _step(self):
+        
+
+        new_cost = self.current_cost
+        while new_cost >= self.current_cost:
+            new_nodes = self._step_trial(self.nodes)
+            result, new_route = self._get_route(new_nodes)
+            if result == 'success':
+                new_cost = self._obj_fn(new_route)
+            print 'try %f --> %f' % (self.current_cost, new_cost)
+        self.current_cost = new_cost
+        self.nodes = new_nodes
+        self.current_route = new_route
 
     def _get_route(self, nodes):
         graph_remaining = self.data.foot_graph.copy()
@@ -89,7 +79,6 @@ class RunRouter:
 
     def find_route(self):
         self._initialize_search()
-        # Take steps following prescribed learning schedule
         while self.current_cost > 800.:
             self._step()
             print 'step - distance'
